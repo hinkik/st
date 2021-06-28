4e8940f21e91eea294fac3aa1ea5782a18e031ba
Henrik Hansson <hinkik@gmail.com>
2021-06-28 17:03:58 +0200

    fix colors

--- config.def.h
+++ config.def.h
@@ -131,10 +131,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultfg = 257;
+static unsigned int defaultcs = 8;
+static unsigned int defaultrcs = 9;
 
 /*
  * Default shape of cursor
